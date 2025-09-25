cff29608350fe1ebb24f1e57bc8c7d399af453f5
Sreeram P <psreeram3015@proton.me>
2025-09-25 22:29:21 +0530

    Created functions to create low pass filter and apply it

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from scipy.fft import fft, ifft, fftfreq
+from . import constants
 
 
 def load_profile_data(filepath):
@@ -9,3 +11,30 @@ def load_profile_data(filepath):
     topography_m = df["topography_m"].values
     bouguer_ms2 = df["bouguer_mgal"].values * 1e-5
     return distance_m, topography_m, bouguer_ms2
+
+
+def create_lowpass_filter(num_points, dx_m):
+    """Creates the modified Hanning window filter described in the paper."""
+    freq = fftfreq(num_points, d=dx_m)
+    wavelength = (
+        np.divide(1.0, np.abs(freq), out=np.full_like(freq, np.inf), where=freq != 0)
+        / 1000
+    )
+
+    filter_window = np.zeros(num_points)
+    filter_window[wavelength > constants.FILTER_WL_START] = 1.0
+
+    mask = (wavelength <= constants.FILTER_WL_START) & (
+        wavelength >= constants.FILTER_WL_END
+    )
+    wl_range = constants.FILTER_WL_START - constants.FILTER_WL_END
+    filter_window[mask] = 0.5 * (
+        1 + np.cos(np.pi * (constants.FILTER_WL_START - wavelength[mask]) / wl_range)
+    )
+
+    return filter_window
+
+
+def apply_filter(data, filter_window):
+    """Applies a filter in the frequency domain."""
+    return np.real(ifft(fft(data) * filter_window))
