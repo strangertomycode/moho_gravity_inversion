26881593d8cc486e80a1d727f73a0a35ac9add85
Sreeram P <psreeram3015@proton.me>
2025-09-25 21:15:58 +0530

    Fixed typo

--- src/constants.py
+++ src/constants.py
@@ -2,9 +2,9 @@
 G = 6.67430e-11
 
 # Standard densities in kg/m^3
-DENCITY_CRUST = 2.67 * 1000
-DENCITY_MANTLE = 3.20 * 1000
-DENCITY_DIFFERENCE = DENCITY_MANTLE - DENCITY_CRUST
+DENSITY_CRUST = 2.67 * 1000
+DENSITY_MANTLE = 3.20 * 1000
+DENSITY_DIFFERENCE = DENSITY_MANTLE - DENSITY_CRUST
 
 # Filter parameters in km, as described in the paper
 FILTER_WL_START = 50.0  # Wavelengths > 50 km have unity gain
